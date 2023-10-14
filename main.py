2435e3bcfbfee5f357075013015f8e2b0b4f33ad
Avi <avanshah@ufl.edu>
2023-10-14 16:47:24 -0400

    fixed red.json and button

--- main.py
+++ main.py
@@ -15,17 +15,6 @@ image_file_path = "first frame.png"
 # Video file path (rename it to your video file)
 video_file_path = "Verizon Intro Better.mp4"
 
-# Function to switch from image to video
-def switch_to_video():
-    # Remove the image label
-    image_label.pack_forget()
-
-    # Create and display the video player
-    videoplayer = TkinterVideo(master=app, scaled=True)
-    videoplayer.load(video_file_path)
-    videoplayer.pack(expand=True, fill="both")
-    videoplayer.play()
-
 # Create a label to display the image
 image = Image.open(image_file_path)
 image = image.resize((1920, 1080))
@@ -33,12 +22,23 @@ photo = ImageTk.PhotoImage(image)
 image_label = tk.Label(app, image=photo)
 image_label.pack(expand=True, fill="both")
 
-# Preload the video in the background
-preload_videoplayer = TkinterVideo(master=app)
-preload_videoplayer.load(video_file_path)
+# Create and configure the video player
+videoplayer = TkinterVideo(master=app, scaled=True)
+videoplayer.load(video_file_path)
+videoplayer.pack(expand=True, fill="both")
 
-# Create a central button to trigger the animation
-button = customtkinter.CTkButton(master=app, text="Play Video", command=switch_to_video)
+# Function to switch from image to video when the button is clicked
+def switch_to_video():
+    # Remove the image label
+    image_label.pack_forget()
+
+    # Play the video
+    videoplayer.play()
+    button.destroy()
+
+# Create a central button to trigger the video transition
+button = customtkinter.CTkButton(master=app, text="Feeling Lost", command=switch_to_video)
+button.configure(height=200, width=400, font=("Roboto", 40))
 button.place(relx=0.5, rely=0.5, anchor="center")
 
 app.mainloop()
